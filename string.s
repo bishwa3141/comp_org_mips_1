4833ab3c7a2b338a9dac8f7a02f330adf82fffb8
3141bishwa <bishwa.slwl@gmail.com>
2017-11-15 19:36:37 -0500

    Added functionality to return the decimal value of the hex string. Also, fixed cases where spaces between characters were previously not showing invalid hex number

--- string.s
+++ string.s
@@ -8,65 +8,76 @@
 .text
 	main:
 		# Code to read input
+		la $s1, 0					# Result
+		la $s3, 0					# Boolean for checking if a space is found after a valid character
+		la $s4, 0					# Boolean for valid character found
 
-		la $s1, 0 							# Boolean for checking if first string
-		la $a0, input_message		#Passing the prompt message address to the register
-		la $a1, 9
+		la $a0, input_message		# Passing the prompt message address to the register
+		la $a1, 9					# Setting the maximum length of the string
 		li $v0, 4					#Syscall code for Printing
-
 		syscall
 
 		# Code to read user input
-		li $v0, 8							#Allocate space for the user input
-		la $`a0, input 				#Save the
-		move $t0, $a0					# Save the string to t0 register
+		li $v0, 8							#	Syscall code for user input
+		la $a0, input 						#Save the
+		move $t0, $a0						# Save the string to t0 register
 
 		syscall
 
 
 
-	loop_through:					#Loops through all the character of the input string one by one
+	loop_through:									#Loops through all the character of the input string one by one
 
-		lb $a0, ($t0)				#Initializing the address for the character of the string
-		add $t0, $t0, 1			#Incrementing the address
+		lb $a0, ($t0)								#Initializing the address for the character of the string
+		
+		add $t0, $t0, 1								#Incrementing the address
 
-		beqz 	$a0, 				end_of_loop			#If "\0" found, end of loop
+		beqz 	$a0, 			end_of_loop			#If "\0" found, end of loop
 
 		beq  	$a0, 	10, 	end_of_loop			#For string less than 9, the last characters
 																			# is "\n", so checking for that
 
-		beq 	$a0, 	32, 	loop_through		#If space character found, ignore it
+		beq 	$a0, 	32, 	space				# Case for when a space character is found
 
 
-		li 		$v0, 	11
-		syscall
+		# Since at this stage, the character can't be a space or '\n' or '\0', we can check if a character
+		# AND a space has appeared before it. If both are true, then it must be invalid. Otherwise, proceed
+		# to computing the decimal value
+		bne $s3, 1, compute							 
+		bne $s4, 1, compute
+
+		j invalid
 
 
+	compute:
+
+		la 	$s4, 1					# Making note that a character has been found.
 
 		blt $a0, 48, invalid 		# checks if the number is less than 48. Goes to invalid if true
 
-		ble $a0, 57, valid			# Checks if the ASCII of character is less than 58. At this point,
+		ble $a0, 57, valid_num			# Checks if the ASCII of character is less than 58. At this point,
 									# already greater than 48, so it is a valid num in the range 0-9
 
 		blt $a0, 65, invalid 		# Checks if the ASCII of character is less than 68. At this point,
 									# already greater than 58, so if true it is an invalid character
 
-		ble $a0, 70, valid			# Checks if the ASCII of character is less than 68. At this point,
+		ble $a0, 70, valid_capital	# Checks if the ASCII of character is less than 68. At this point,
 									# already greater than 61, so if true it is a valid char in the range A - F
 
 		blt $a0, 96, invalid 		# Checks if the ASCII of character is less than 96. At this point,
 									# already greater than 70, so if true it is an invalid character
 
-		ble $a0, 102, valid			# Checks if the ASCII of character is less than 58. At this point,
+		ble $a0, 102, valid_small	# Checks if the ASCII of character is less than 58. At this point,
 									# already greater than 96, so if true, it is a valid num in the range a-f
 
 		j invalid 					#At this point, already greater than 103, so it is invalid
 
-
 	end_of_loop:
 
-		la $a0, end_msg
-		li $v0, 4
+		beqz $s4, invalid 			#If no valid characters found by the end of the loop, treat it as invalid
+
+		li $v0, 1					# Syscall code for printing out an integer
+		move $a0, $s1
 		syscall
 
 		li	$v0, 10		# system call code for exit = 10
@@ -75,18 +86,43 @@
 
 	invalid:
 		# Printing the given message
-		la $a0, err_msg
+		la $a0, err_msg	
 		li $v0, 4
 		syscall
 
 		li	$v0, 10		# system call code for exit = 10
 		syscall
 
+	valid_num:
 
-	valid:
-
-		la $a0, valid_char_message
-		li $v0, 4
-		syscall
+		sll 	$s1,	$s1, 	4 				# Multiplying the result by 16 using bit-shift	
+		subu 	$s2, 	$a0, 	48				# Finding the real value of the character by subtracting
+		addu 	$s1,	$s1, 	$s2 			# Adding the value of the character to the result
 
 		j loop_through
+
+	valid_capital:
+
+
+		sll 	$s1,	$s1, 	4				# Multiplying the result by 16 using bit-shift
+		subu 	$s2, 	$a0, 	55				# Finding the real value of the character by subtracting
+		addu 	$s1,	$s1, 	$s2				# Adding the value of the character to the result
+
+		j loop_through
+
+
+	valid_small:
+
+		sll 	$s1,	$s1, 	4				# Multiplying the result by 16 using bit-shift
+		subu 	$s2, 	$a0, 	87				# Finding the real value of the character by subtracting
+		addu 	$s1,	$s1, 	$s2				# Adding the value of the character to the result
+
+		j loop_through
+
+	space:
+		beqz	$s4, loop_through			# Checks if a character has already been found
+
+		la 	$s3, 1							# Making note of a space found after a character is found
+
+		j loop_through
+
